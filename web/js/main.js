ddef2bb8dbdded2968c960dcc4852320600bcf5f
Luqmaan <ldawoodjee@gmail.com>
2013-05-10 19:41:43 -0400

    Animating player

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,30 @@
 
+var $player = $(".player");
+var $track = $(".track");
+var $progress = $(".progress");
+var playerWidth = $player.width();
+
+var at = 0;
+var total = 100;
+
+
+function draw(ratio) {
+    var w = playerWidth * ratio;
+    $progress.width(w + "px");
+    console.log("hai");
+}
+
+function update() {
+    if (at === total)
+        at = 0;
+    else
+        at++;
+    draw(at/total);
+    setTimeout(update, 100);
+}
+
+function init() {
+    update();
+}
+
+init();
