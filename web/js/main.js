ec9a0a72459eca2b445dbf1db4686afa2e81af92
Luqmaan <ldawoodjee@gmail.com>
2013-05-12 19:53:42 -0400

    Added media player. Need to check ie compatibility. Site looks great even in IE7

--- web/js/main.js
+++ web/js/main.js
@@ -1,30 +1,122 @@
+/*
+    Misc
+ */
+(function($) {
 
-var $player = $(".player");
-var $track = $(".track");
-var $progress = $(".progress");
-var playerWidth = $player.width();
+    resize() {
+        // firefox fix
+        $(".jenna").height($(window).height() + "px")
+    }
 
-var at = 0;
-var total = 100;
+    resize()
+    $(window).resize(resize())
 
+}(jQuery));
+/*
+    Audio player
+ */
+(function($) {
 
-function draw(ratio) {
-    var w = playerWidth * ratio;
-    $progress.width(w + "px");
-    console.log("hai");
-}
+    var playlist, playlistPos // songs, acquired from DOM
+    var $audio, audio // audio player
+    var player, at, total, updateTrack // audio player + progress
+    var $progress, playerWidth, $control // UI stuff
 
-function update() {
-    if (at === total)
-        at = 0;
-    else
-        at++;
-    draw(at/total);
-    setTimeout(update, 100);
-}
+    function draw(ratio) {
+        var w = playerWidth * ratio
+        $progress.width(w + "px")
+    }
 
-function init() {
-    update();
-}
+    function update() {
+        total = player.duration
+        if (at === total) {
+            at = 0
+            clearInterval()
+            nextSong()
+        }
+        else
+            at = player.currentTime
+        draw(at/total)
+    }
 
-init();
+    function play() {
+        $control.children().removeClass("play").addClass("pause");
+        player.play()
+    }
+
+    function pause() {
+        $control.children().removeClass("pause").addClass("play");
+        player.pause()
+    }
+
+    function togglePause() {
+        console.log("toggle")
+        if (player.paused)
+            play()
+        else
+            pause()
+    }
+
+    function playSong(url) {
+
+        $audio = $("<audio id='mediaelement' autoplay='false' src='" + url + "' type='audio/mp3'></audio>")
+        audio = $audio[0]
+
+        var el = new MediaElement(audio, {
+            startVolume: 1,
+            timerRate: 250,
+            plugins: ['flash','silverlight'],
+            success: function(mediaelement) {
+                player = mediaelement
+                at = 0
+                total = player.duration
+                play()
+                updateTrack = setInterval(update, 250)
+            }
+        })
+
+    }
+
+    function nextSong() {
+        // stop the playing song if we have started one
+        if (typeof $audio !== "undefined") {
+            player.pause()
+            $audio.remove()
+            playlistPos++
+        }
+
+        if (playlistPos == playlist.length)
+            playlistPos++
+
+        var song = playlist[playlistPos]
+
+        $(".title-text").text(song.title)
+        $(".song").text(song.title)
+
+        console.log(song)
+
+        playSong(song.url)
+    }
+
+    function init() {
+        console.log("Did init")
+        $progress = $(".progress")
+        $control = $(".control")
+        playerWidth = $(".player").width()
+        playlistPos = 0 
+        playlist = []
+
+        $control.on("click", togglePause)
+
+        $(".playlist li").each(function() {
+            playlist.push({
+                url: $(this).data("url"),
+                title: $(this).data("title")
+            })
+        })
+        nextSong()
+    }
+
+    init()
+
+}(jQuery));
